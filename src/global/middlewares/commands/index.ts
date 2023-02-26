680c4b39328f738483b7efd380d3f181df8de325
sailendra dash <sailendradash@protonmail.com>
2023-02-26 14:17:13 +0530

    JSON view triggered

--- src/global/middlewares/commands/index.ts
+++ src/global/middlewares/commands/index.ts
@@ -1,12 +1,17 @@
 import { IreceiveMessage } from "@src/interfaces";
 import { TS_TREE_COMMAND } from "@src/types/tree";
 import ts_tree_command from "./functions/ts_tree_command";
+import { EXECUTE } from "@src/types/custom";
+import execute_command from "./functions/execute_command";
 
 export default function exeCommands(data: IreceiveMessage) {
     const { command, data: value } = data;
     switch (command) {
         case TS_TREE_COMMAND:
             ts_tree_command()
+            break;
+        case EXECUTE:
+            execute_command(value)
             break;
         default:
             break;
---
+++ src/types/custom.ts
@@ -0,0 +1,1 @@
+export const EXECUTE = "EXECUTE"
